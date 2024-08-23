46dd2f7486d2844a6c68bc6cf6892690496c8678
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-08-23 12:34:39 +0200

    Update ElementoCatalogo.java
    Creazione classe astratta

--- src/main/java/UmbertoAmoroso/Entities/ElementoCatalogo.java
+++ src/main/java/UmbertoAmoroso/Entities/ElementoCatalogo.java
@@ -1,4 +1,20 @@
 package UmbertoAmoroso.Entities;
+import jakarta.persistence.*;
 
-public class ElementoCatalogo {
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_elemento")
+public abstract class ElementoCatalogo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String isbn;
+    private String titolo;
+    private int annoPubblicazione;
+    private int numeroPagine;
+
 }
